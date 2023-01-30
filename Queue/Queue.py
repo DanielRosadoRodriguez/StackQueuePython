d76b301b327cfb58c64fcc3df2b31655623206d5
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2023-01-29 18:11:02 -0600

    add Queue class and exception

---
+++ Queue/Queue.py
@@ -0,0 +1,20 @@
+from exceptions import EmptyQueueException
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise EmptyQueueException("Queue is empty")
+
+    def size(self):
+        return len(self.items)
